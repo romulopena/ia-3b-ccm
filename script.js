a5430aa93e8438ccedde7a4f9041b2664356609f
PROF ROMULO <rjlpena@gmail.com>
2025-05-28 13:40:56 +0000

    mostraAlternativa

--- script.js
+++ script.js
@@ -6,10 +6,52 @@ const textoResultado = document.querySelector(".texto-resultado");
 const perguntas = [
     {
         enunciado: "Pergunta1",
-        alternativas: ["alternativa1","alternativa2"]
+        alternativas: [
+            {
+                texto: "alternativa1",
+                afirmacao: "afirmacao1",
+            },
+            {
+                texto: "alternativa2",
+                afirmacao: "afirmacao2",
+            },
+        ]
     },
     {
         enunciado: "Pergunta2",
-        alternativas: ["alternativa1","alternativa2"]
+        alternativas: [
+            {
+                texto: "alternativa1",
+                afirmacao: "afirmacao1",
+            },
+            {
+                texto: "alternativa2",
+                afirmacao: "afirmacao2",
+            },
+        ]
+    },
+    {
+        enunciado: "Pergunta3",
+        alternativas: [
+            {
+                texto: "alternativa1",
+                afirmacao: "afirmacao1",
+            },
+            {
+                texto: "alternativa2",
+                afirmacao: "afirmacao2",
+            },
+        ]
     },
 ]
+let atual = 0;
+let perguntaAtual;
+function mostraPergunta(){
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
+function mostraAlternativas(){
+    //falta esses codigos
+}
+mostraPergunta();
